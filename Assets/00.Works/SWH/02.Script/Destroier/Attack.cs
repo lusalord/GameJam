daf14e50b6d705ec6056e5373f240531414015d9
songyunhyun <sans10qp10qp@gmail.com>
2025-12-22 23:37:03 +0900

    feat : None

--- Assets/00.Works/SWH/02.Script/Destroier/Attack.cs
+++ Assets/00.Works/SWH/02.Script/Destroier/Attack.cs
@@ -4,6 +4,9 @@ public class Attack : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //if(collision.gameObject.try)
+        if (collision.gameObject.TryGetComponent(out EnemyHealthSystem eh))
+        {
+            eh.Hp--;
+        }
     }
 }
